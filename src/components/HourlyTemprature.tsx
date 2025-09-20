3407604f72e5e9bcb758966384fc6a37cdbd0518
Jibinnn12 <jibcr07@gmail.com>
2025-09-21 00:19:11 +0530

    Fixed bugs for deployment

--- src/components/HourlyTemprature.tsx
+++ src/components/HourlyTemprature.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import  { useMemo, useState } from "react";
 import type { ForecastData } from "@/api/types";
 // Import your Card, CardContent, CardHeader etc. as before
 import {
